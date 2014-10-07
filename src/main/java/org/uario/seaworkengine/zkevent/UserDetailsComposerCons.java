bf1647e04fd727dab7b5034afb7610ed8605a9bc
fdamore <francesco.damore@gmail.com>
2014-10-07 13:34:00 +0200

    Fixed issue SEA-28

--- src/main/java/org/uario/seaworkengine/zkevent/UserDetailsComposerCons.java
+++ src/main/java/org/uario/seaworkengine/zkevent/UserDetailsComposerCons.java
@@ -1,14 +1,23 @@
 package org.uario.seaworkengine.zkevent;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
 import java.util.Calendar;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 import org.uario.seaworkengine.model.Contestation;
 import org.uario.seaworkengine.model.Person;
 import org.uario.seaworkengine.platform.persistence.dao.IContestation;
+import org.uario.seaworkengine.platform.persistence.dao.IParams;
 import org.uario.seaworkengine.utility.BeansTag;
 import org.uario.seaworkengine.utility.ContestationTag;
+import org.uario.seaworkengine.utility.ParamsTag;
 import org.uario.seaworkengine.utility.ZkEventsTag;
 import org.zkoss.spring.SpringUtil;
 import org.zkoss.zk.ui.Component;
@@ -18,8 +27,10 @@ import org.zkoss.zk.ui.event.UploadEvent;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.Button;
 import org.zkoss.zul.Combobox;
 import org.zkoss.zul.Datebox;
+import org.zkoss.zul.Filedownload;
 import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Listbox;
 import org.zkoss.zul.Messagebox;
@@ -27,28 +38,69 @@ import org.zkoss.zul.Textbox;
 
 public class UserDetailsComposerCons extends SelectorComposer<Component> {
 
+	private class ContestationDoc {
+		/**
+		 * The file to add
+		 */
+		private byte[]	file_doc;
+
+		private String	format;
+
+		public byte[] getFile_doc() {
+			return this.file_doc;
+		}
+
+		public String getFormat() {
+			return this.format;
+		}
+
+		public void setFile_doc(final byte[] file_doc) {
+			this.file_doc = file_doc;
+		}
+
+		public void setFormat(final String format) {
+			this.format = format;
+		}
+
+	}
+
+	private final static String	BUTTON_FINAL_SCLASS		= "btn-danger";
+
+	private final static String	BUTTON_INITIAL_SCLASS	= "btn-success";
+
 	/**
 	 *
 	 */
-	private static final long	serialVersionUID	= 1L;
+	private static final long	serialVersionUID		= 1L;
 
 	// dao interface
 	private IContestation		contestationDAO;
 
 	@Wire
+	private Component			current_document;
+
+	private ContestationDoc		currentDoc				= null;
+
+	@Wire
 	private Datebox				date_contestation;
 
 	@Wire
+	private Button				docupload;
+
+	@Wire
 	private Component			grid_details;
-	private final Logger		logger				= Logger.getLogger(UserDetailsComposerCons.class);
+
+	private final Logger		logger					= Logger.getLogger(UserDetailsComposerCons.class);
 
 	@Wire
 	private Textbox				note;
 
+	private IParams				paramsDAO;
+
 	private Person				person_selected;
 
 	// status ADD or MODIFY
-	private boolean				status_add			= false;
+	private boolean				status_add				= false;
 
 	@Wire
 	private Datebox				stop_from;
@@ -78,6 +130,15 @@ public class UserDetailsComposerCons extends SelectorComposer<Component> {
 
 		this.status_add = true;
 
+		// set null current contestaion doc
+		this.currentDoc = null;
+
+		// set link to current document
+		this.current_document.setVisible(false);
+
+		// set initial sclass
+		this.docupload.setSclass(UserDetailsComposerCons.BUTTON_INITIAL_SCLASS);
+
 	}
 
 	@Listen("onChange = #typ")
@@ -106,7 +167,17 @@ public class UserDetailsComposerCons extends SelectorComposer<Component> {
 		final Contestation item = this.sw_list.getSelectedItem().getValue();
 
 		this.contestationDAO.removeContestation(item.getId());
+
 		Messagebox.show("Informazione rimossa", "INFO", Messagebox.OK, Messagebox.INFORMATION);
+
+		// remove doc
+		if (item.getFile_name() != null) {
+			final String repo = this.paramsDAO.getParam(ParamsTag.REPO_DOC);
+			final String global_file_name = repo + item.getFile_name();
+			final File file = new File(global_file_name);
+			file.delete();
+
+		}
 
 		// Refresh list task
 		this.setInitialView();
@@ -129,12 +200,36 @@ public class UserDetailsComposerCons extends SelectorComposer<Component> {
 
 				// get the dao
 				UserDetailsComposerCons.this.contestationDAO = (IContestation) SpringUtil.getBean(BeansTag.CONTESTATION_DAO);
+				UserDetailsComposerCons.this.paramsDAO = (IParams) SpringUtil.getBean(BeansTag.PARAMS_DAO);
 
 				UserDetailsComposerCons.this.setInitialView();
 
 			}
 		});
 
+	}
+
+	@Listen("onClick = #sw_download_list, #current_document")
+	public void downloadDocProcedure() throws FileNotFoundException {
+		if (this.sw_list.getSelectedItem() == null) {
+			return;
+		}
+
+		final Contestation item = this.sw_list.getSelectedItem().getValue();
+
+		if (item.getFile_name() == null) {
+			Messagebox.show("Nessun documento associato", "INFO", Messagebox.OK, Messagebox.EXCLAMATION);
+			return;
+		}
+
+		// download
+
+		final String repo = this.paramsDAO.getParam(ParamsTag.REPO_DOC);
+		final String global_file_name = repo + item.getFile_name();
+		final File file = new File(global_file_name);
+		if (file.exists()) {
+			Filedownload.save(file, null);
+		}
 	}
 
 	@Listen("onClick = #sw_link_edit")
@@ -160,13 +255,28 @@ public class UserDetailsComposerCons extends SelectorComposer<Component> {
 		this.stop_to.setValue(item.getStop_to());
 		this.typ.setValue(item.getTyp());
 
+		// set null current contestaion doc
+		this.currentDoc = null;
+
+		// set link to current document
+		if (item.getFile_name() == null) {
+			this.current_document.setVisible(false);
+		}
+		else {
+			this.current_document.setVisible(true);
+
+		}
+
+		// set initial sclass
+		this.docupload.setSclass(UserDetailsComposerCons.BUTTON_INITIAL_SCLASS);
+
 		// set date box
 		this.setDateBoxs();
 
 	}
 
 	@Listen("onClick = #ok_command")
-	public void okCommand() {
+	public void okCommand() throws IOException {
 
 		if (this.person_selected == null) {
 			return;
@@ -210,7 +320,8 @@ public class UserDetailsComposerCons extends SelectorComposer<Component> {
 
 			Messagebox.show("Contestazione aggiunta all'utente", "INFO", Messagebox.OK, Messagebox.INFORMATION);
 
-		} else {
+		}
+		else {
 
 			if (this.typ.getSelectedItem() == null) {
 				Messagebox.show("Inserire un tipo di contestazione!");
@@ -262,7 +373,8 @@ public class UserDetailsComposerCons extends SelectorComposer<Component> {
 			this.stop_from.setDisabled(false);
 			this.stop_to.setDisabled(false);
 
-		} else {
+		}
+		else {
 			this.stop_from.setDisabled(true);
 			this.stop_to.setDisabled(true);
 			this.stop_from.setValue(null);
@@ -282,23 +394,120 @@ public class UserDetailsComposerCons extends SelectorComposer<Component> {
 		this.sw_list.setModel(new ListModelList<Contestation>(list));
 
 		this.grid_details.setVisible(false);
+
+		// set null current contestaion doc
+		this.currentDoc = null;
+
+		// set link to current docuemnt
+		this.current_document.setVisible(false);
 	}
 
-	private void setupItemWithValues(final Contestation item) {
+	/**
+	 * Set value
+	 *
+	 * @param item
+	 * @throws FileNotFoundException
+	 */
+	private void setupItemWithValues(final Contestation item) throws IOException {
 		item.setDate_contestation(this.date_contestation.getValue());
 		item.setNote(this.note.getValue());
 		item.setStop_from(this.stop_from.getValue());
 		item.setStop_to(this.stop_to.getValue());
 		item.setTyp(this.typ.getValue());
+
+		// set file name
+		if (this.currentDoc != null) {
+			final String repo = this.paramsDAO.getParam(ParamsTag.REPO_DOC);
+			final String filename = "" + System.currentTimeMillis() + "." + this.currentDoc.getFormat();
+
+			final String global_file_name = repo + filename;
+
+			FileOutputStream stream = null;
+			try {
+
+				stream = new FileOutputStream(global_file_name);
+				IOUtils.write(this.currentDoc.getFile_doc(), stream);
+
+			}
+			finally {
+				if (stream != null) {
+					try {
+						stream.close();
+					}
+					catch (final IOException ignore) {
+
+					}
+				}
+			}
+
+			item.setFile_name(filename);
+		}
+
 	}
 
-	@Listen("onUpload = #doc")
-	public void uploadFile(final UploadEvent evt) {
+	@Listen("onUpload = #docupload")
+	public void uploadFile(final UploadEvent evt) throws IOException {
 
-		final String nameFile = evt.getName();
+		Reader reader = null;
+		InputStream stream = null;
 
-		final int i = 0;
+		final String format = evt.getMedia().getFormat();
+		byte[] byteDoc = null;
 
+		try {
+
+			if (!evt.getMedia().isBinary()) {
+				if (evt.getMedia().inMemory()) {
+
+					final String info = evt.getMedia().getStringData();
+					byteDoc = info.getBytes();
+
+				}
+				else {
+					reader = evt.getMedia().getReaderData();
+					byteDoc = IOUtils.toByteArray(reader);
+
+				}
+			}
+			else {
+				if (evt.getMedia().inMemory()) {
+
+					byteDoc = evt.getMedia().getByteData();
+
+				}
+				else {
+
+					stream = evt.getMedia().getStreamData();
+					byteDoc = IOUtils.toByteArray(stream);
+
+				}
+			}
+
+			this.currentDoc = new ContestationDoc();
+			this.currentDoc.setFile_doc(byteDoc);
+			this.currentDoc.setFormat(format);
+
+			// set initial sclass
+			this.docupload.setSclass(UserDetailsComposerCons.BUTTON_FINAL_SCLASS);
+
+		}
+		finally {
+
+			if (reader != null) {
+				try {
+					reader.close();
+
+					if (stream != null) {
+						stream.close();
+					}
+				}
+
+				catch (final IOException ignore) {
+
+				}
+
+			}
+
+		}
 	}
-
 }
