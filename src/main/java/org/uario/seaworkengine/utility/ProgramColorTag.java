2a6a1941bf94cf19e5e9eac9f44cb528109a3d36
Gabriele Lanza <gabrielelanza@gmail.com>
2014-10-29 18:52:25 +0100

    Changed colors in interface ProgramColorTag - Improving SEA-74

--- src/main/java/org/uario/seaworkengine/utility/ProgramColorTag.java
+++ src/main/java/org/uario/seaworkengine/utility/ProgramColorTag.java
@@ -2,8 +2,8 @@ package org.uario.seaworkengine.utility;
 
 public interface ProgramColorTag {
 
-	public static String	COLOR_FORZABLE		= "#87B87F";
+	public static String	COLOR_FORZABLE		= "#FFB752";
 	public static String	COLOR_NOT_AVAILABLE	= "#D15B47";
-	public static String	COLOR_WORKER		= "#FFB752";
+	public static String	COLOR_WORKER		= "#87B87F";
 
 }
