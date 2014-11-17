59bb17e69d69d67741386851aa4bccf60f6548ef
fdamore <francesco.damore@gmail.com>
2014-11-17 10:52:28 +0100

    Fix SEA-114.

--- src/main/java/org/uario/seaworkengine/platform/persistence/mybatis/MyBatisStatisticsDAO.java
+++ src/main/java/org/uario/seaworkengine/platform/persistence/mybatis/MyBatisStatisticsDAO.java
@@ -13,21 +13,24 @@ import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.uario.seaworkengine.platform.persistence.dao.IStatistics;
 import org.uario.seaworkengine.statistics.AverageShift;
 
-public class MyBatisStatisticsDAO extends SqlSessionDaoSupport implements
-		IStatistics {
-	private static Logger logger = Logger.getLogger(MyBatisStatisticsDAO.class);
+public class MyBatisStatisticsDAO extends SqlSessionDaoSupport implements IStatistics {
+	private static Logger	logger	= Logger.getLogger(MyBatisStatisticsDAO.class);
 
 	@Override
-	public List<AverageShift> getAverageForShift(final Integer user,
-			final Date date) {
+	public List<AverageShift> getAverageForShift(final Integer user, final Date date) {
 		MyBatisStatisticsDAO.logger.info("loadTFRByUser..");
 
 		final HashMap<String, Object> map = new HashMap<String, Object>();
 		map.put("id_user", user);
 		map.put("date_schedule", date);
 
-		final List<AverageShift> lists = this.getSqlSession().selectList(
-				"statistics.selectAvgShiftByUser", map);
+		List<AverageShift> lists = null;
+
+		lists = this.getSqlSession().selectList("statistics.selectAvgShiftByUserReviewd", map);
+		if (lists == null) {
+			lists = this.getSqlSession().selectList("statistics.selectAvgShiftByUserProgram", map);
+		}
+
 		return lists;
 	}
 
