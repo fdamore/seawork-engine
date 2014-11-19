ecca6593e0c3646910c9db0be193429d145973a5
Francesco D'Amore <f.damore@uario.com>
2014-11-19 15:48:08 +0100

    Improving SEA-118

--- src/main/java/org/uario/seaworkengine/zkevent/ShipSchedulerComposer.java
+++ src/main/java/org/uario/seaworkengine/zkevent/ShipSchedulerComposer.java
@@ -11,33 +11,11 @@ public class ShipSchedulerComposer extends SelectorComposer<Component> {
 	@Override
 	public void doFinally() throws Exception {
 
-		this.getSelf().addEventListener(ZkEventsTag.onShowScheduler, new EventListener<Event>() {
-
-			@Override
-			public void onEvent(final Event arg0) throws Exception {
-
-			}
-		});
-
 		// SHOW SHIFT CONFIGURATOR
 		this.getSelf().addEventListener(ZkEventsTag.onShowShipScheduler, new EventListener<Event>() {
 
 			@Override
 			public void onEvent(final Event arg0) throws Exception {
-
-				// final String data_info = arg0.getData().toString();
-
-			}
-
-		});
-
-		// SHOW SHIFT CONFIGURATOR
-		this.getSelf().addEventListener(ZkEventsTag.onShiftClickReview, new EventListener<Event>() {
-
-			@Override
-			public void onEvent(final Event arg0) throws Exception {
-
-				final String data_info = arg0.getData().toString();
 
 			}
 
