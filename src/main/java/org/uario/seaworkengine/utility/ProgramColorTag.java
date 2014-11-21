fc6bd2a04212d8f4fd90d4d3cfca20217c585470
Gabriele Lanza <gabrielelanza@gmail.com>
2014-11-21 12:12:01 +0100

    SEA-121

--- src/main/java/org/uario/seaworkengine/utility/ProgramColorTag.java
+++ src/main/java/org/uario/seaworkengine/utility/ProgramColorTag.java
@@ -4,7 +4,7 @@ public interface ProgramColorTag {
 
 	public static String	COLOR_FORZABLE		= "#FFB752";
 	public static String	COLOR_NOT_AVAILABLE	= "#D15B47";
-	public static String	COLOR_WORKER		= "#FFFFFF";
+	public static String	COLOR_WORKER		= "#91B98A";
 	public static String	DISABLE_COLOR		= "#FFFFFF";
 
 }
