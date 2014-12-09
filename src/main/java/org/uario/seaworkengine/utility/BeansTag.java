9434d6647e950bf44ef23bfc4fc753bf448f0b47
Gabriele Lanza <gabrielelanza@gmail.com>
2014-12-09 13:01:09 +0100

    Removed bean DETAIL_SCHEDULE_SHIP_DAO SEA-140

--- src/main/java/org/uario/seaworkengine/utility/BeansTag.java
+++ src/main/java/org/uario/seaworkengine/utility/BeansTag.java
@@ -2,42 +2,40 @@ package org.uario.seaworkengine.utility;
 
 public interface BeansTag {
 
-	public static String	ASSIGN_SHIFT_DATE_BEAN		= "serviceAssign";
+	public static String	ASSIGN_SHIFT_DATE_BEAN	= "serviceAssign";
 
-	public static String	CONFIGURATION_DAO			= "myConfigurationDAO";
+	public static String	CONFIGURATION_DAO		= "myConfigurationDAO";
 
-	public static String	CONTESTATION_DAO			= "myContestationDAO";
+	public static String	CONTESTATION_DAO		= "myContestationDAO";
 
-	public static String	DETAIL_SCHEDULE_SHIP_DAO	= "myDetailScheduleShipDAO";
+	public static String	EMPLOYMENT_DAO			= "myEmploymentDAO";
 
-	public static String	EMPLOYMENT_DAO				= "myEmploymentDAO";
+	public static String	FISCAL_CONTROL_DAO		= "myFiscalControlDAO";
 
-	public static String	FISCAL_CONTROL_DAO			= "myFiscalControlDAO";
+	public static String	JOB_COST_DAO			= "myJobCostDAO";
 
-	public static String	JOB_COST_DAO				= "myJobCostDAO";
+	public static String	PARAMS_DAO				= "myParamDAO";
 
-	public static String	PARAMS_DAO					= "myParamDAO";
+	public static String	PERSON_DAO				= "myPersonDAO";
 
-	public static String	PERSON_DAO					= "myPersonDAO";
+	public static String	SCHEDULE_DAO			= "myScheduleDAO";
 
-	public static String	SCHEDULE_DAO				= "myScheduleDAO";
+	public static String	SCHEDULE_SHIP_DAO		= "myScheduleShipDAO";
 
-	public static String	SCHEDULE_SHIP_DAO			= "myScheduleShipDAO";
+	public static String	SHIFT_CACHE				= "myShiftCache";
 
-	public static String	SHIFT_CACHE					= "myShiftCache";
+	public static String	SHIP_DAO				= "myShipDAO";
 
-	public static String	SHIP_DAO					= "myShipDAO";
+	public static String	STAT_PROCEDURE			= "myStatProcedure";
 
-	public static String	STAT_PROCEDURE				= "myStatProcedure";
+	public static String	STATISTICS				= "myStatisticsDAO";
 
-	public static String	STATISTICS					= "myStatisticsDAO";
+	public static String	TASK_CACHE				= "myTaskCache";
 
-	public static String	TASK_CACHE					= "myTaskCache";
+	public static String	TASK_DAO				= "myTasksDAO";
 
-	public static String	TASK_DAO					= "myTasksDAO";
+	public static String	TFR_DAO					= "myTfrDAO";
 
-	public static String	TFR_DAO						= "myTfrDAO";
-
-	public static String	TRADE_UNION_DAO				= "myTradeUnionDAO";
+	public static String	TRADE_UNION_DAO			= "myTradeUnionDAO";
 
 }
