effd0c3e61a1074d0b47c0dbfbb744d2cac83c9c
Gabriele Lanza <gabrielelanza@gmail.com>
2014-12-19 11:21:30 +0100

    Changed daily color shift - SEA-186

--- src/main/java/org/uario/seaworkengine/utility/ProgramColorTag.java
+++ src/main/java/org/uario/seaworkengine/utility/ProgramColorTag.java
@@ -5,7 +5,7 @@ public interface ProgramColorTag {
 	public static String	COLOR_FORZABLE		= "#FFB752";
 	public static String	COLOR_NOT_AVAILABLE	= "#D15B47";
 	public static String	COLOR_WORKER		= "#91B98A";
-	public static String	DAILY_COLOR			= "red";
+	public static String	DAILY_COLOR			= "#8E8EA5";
 	public static String	DISABLE_COLOR		= "#FFFFFF";
 
 }
