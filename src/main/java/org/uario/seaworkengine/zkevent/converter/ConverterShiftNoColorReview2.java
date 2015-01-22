30849d59e4f392e1b93c39e0814d828734abe781
Gabriele Lanza <gabrielelanza@gmail.com>
2015-01-22 18:35:12 +0100

    Completed SEA-222

--- src/main/java/org/uario/seaworkengine/zkevent/converter/ConverterShiftNoColorReview2.java
+++ src/main/java/org/uario/seaworkengine/zkevent/converter/ConverterShiftNoColorReview2.java
@@ -1,9 +1,13 @@
 package org.uario.seaworkengine.zkevent.converter;
 
+import org.uario.seaworkengine.model.UserShift;
+import org.uario.seaworkengine.utility.ShiftTag;
 import org.uario.seaworkengine.zkevent.bean.ItemRowSchedule;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zkplus.databind.TypeConverter;
 import org.zkoss.zul.Listcell;
+import org.zkoss.zul.Listitem;
+import org.zkoss.zul.Toolbarbutton;
 
 public class ConverterShiftNoColorReview2 implements TypeConverter {
 
@@ -23,6 +27,36 @@ public class ConverterShiftNoColorReview2 implements TypeConverter {
 		}
 
 		final ItemRowSchedule item_schedule = (ItemRowSchedule) arg0;
+
+		// get status
+		final String status = UtilityProgramRow.getCurrentStatus(item_schedule);
+
+		// define behavior
+		UtilityProgramRow.defineRowBeahvior(arg1, item_schedule, status);
+
+		// get user shift
+		final UserShift myShift = UtilityProgramRow.getCurrentUserShift(item_schedule);
+
+		// return info
+		if (status != null) {
+			if (status.equals(ShiftTag.USER_WORKER_NOT_AVAILABLE) || status.equals(ShiftTag.USER_WORKER_FORZABLE)) {
+				if ((item_schedule.getSchedule() != null) && (item_schedule.getSchedule().getShift() != null)) {
+					if (myShift != null) {
+						final Toolbarbutton toolbarbutton = (Toolbarbutton) arg1;
+						toolbarbutton.setDisabled(true);
+
+						final Listitem listItem = (Listitem) arg1.getParent().getParent();
+						listItem.setDisabled(true);
+						listItem.setCheckable(false);
+
+						return myShift.getCode();
+					} else {
+						return UtilityProgramRow.NO_DATA;
+					}
+
+				}
+			}
+		}
 
 		if (item_schedule.getAnchor2() == null) {
 			final Component comp = arg1;
