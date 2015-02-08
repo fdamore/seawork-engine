15f39ccba92e023e91379142d6b0b43cea257526
Francesco D'Amore <francesco.damore@gmail.com>
2015-02-08 12:51:16 +0100

    Improving SEA-201

--- src/main/java/org/uario/seaworkengine/zkevent/converter/BankHolidayConverter.java
+++ src/main/java/org/uario/seaworkengine/zkevent/converter/BankHolidayConverter.java
@@ -1,15 +1,16 @@
 package org.uario.seaworkengine.zkevent.converter;
 
-import java.util.Arrays;
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zkplus.databind.TypeConverter;
 
 public class BankHolidayConverter implements TypeConverter {
 
-	List<String>	Month	= Arrays.asList("Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre",
-									"Novembre", "Dicembre");
+	private static SimpleDateFormat format_input = new SimpleDateFormat("MM-dd");
+	private static SimpleDateFormat format_output = new SimpleDateFormat("dd MMMMM");
 
 	@Override
 	public Object coerceToBean(final Object arg0, final Component arg1) {
@@ -20,17 +21,47 @@ public class BankHolidayConverter implements TypeConverter {
 	@Override
 	public Object coerceToUi(final Object arg0, final Component arg1) {
 
-		if (!(arg0 instanceof String) || (arg0 == null)) {
-			return arg0;
+		try {
+			if (!(arg0 instanceof String) || (arg0 == null)) {
+				return arg0;
+			}
+
+			final String dt = (String) arg0;
+
+			final Date date_input = BankHolidayConverter.format_input.parse(dt);
+			String date_info = BankHolidayConverter.format_output.format(date_input);
+
+			String name = "";
+			if (dt.equals("01-01")) {
+				name = " (Capodanno)";
+			} else if (dt.equals("01-06")) {
+				name = " (Epifania)";
+			} else if (dt.equals("04-25")) {
+				name = " (Liberazione)";
+			} else if (dt.equals("05-01")) {
+				name = " (Festa del Lavoro)";
+			} else if (dt.equals("06-02")) {
+				name = " (Festa della Repubblica)";
+			} else if (dt.equals("08-15")) {
+				name = " (Ferragosto)";
+			} else if (dt.equals("09-01")) {
+				name = " (Ognissanti)";
+			} else if (dt.equals("12-08")) {
+				name = " (Ognissanti)";
+			} else if (dt.equals("12-25")) {
+				name = " (Natale)";
+			} else if (dt.equals("12-26")) {
+				name = " (Santo Stefano)";
+			} else if (dt.equals("08-13")) {
+				name = " (Festa Padronale)";
+			}
+
+			date_info = date_info + name;
+
+			return date_info;
+		} catch (final ParseException e) {
+			return "";
 		}
-
-		final String dt = (String) arg0;
-
-		final int month = Integer.parseInt(dt.substring(0, 2));
-
-		final String date = dt.substring(3, 5) + " " + this.Month.get(month - 1);
-
-		return date;
 
 	}
 }
