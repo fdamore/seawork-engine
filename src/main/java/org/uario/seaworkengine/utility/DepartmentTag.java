ac7a0e078cd7348e9fd8e9513b62fa083aeb85ec
Gabriele Lanza <gabrielelanza@gmail.com>
2015-05-08 18:49:34 +0200

    Completed SEA-444

--- src/main/java/org/uario/seaworkengine/utility/DepartmentTag.java
+++ src/main/java/org/uario/seaworkengine/utility/DepartmentTag.java
@@ -2,6 +2,7 @@ package org.uario.seaworkengine.utility;
 
 public interface DepartmentTag {
 
+	// tag department
 	public static String	ADMINISTRATION	= "AMMINISTRAZIONE";
 
 	public static String	OPERATIVE		= "OPERATIVO";
