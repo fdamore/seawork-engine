10fe75c0e6eb394d9e409dd1352b41e54e0d3d02
Gabriele Lanza <gabrielelanza@gmail.com>
2015-05-21 17:50:33 +0200

    Change iddent after eclipse reconf

--- src/main/java/org/uario/seaworkengine/zkevent/converter/ConverterShipName.java
+++ src/main/java/org/uario/seaworkengine/zkevent/converter/ConverterShipName.java
@@ -1,14 +1,12 @@
 package org.uario.seaworkengine.zkevent.converter;
 
 import org.uario.seaworkengine.model.Ship;
-import org.uario.seaworkengine.zkevent.bean.RowSchedule;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zkplus.databind.TypeConverter;
-import org.zkoss.zul.Listcell;
 
 public class ConverterShipName implements TypeConverter {
 
-	private static final String	NO_DATA	= "_";
+	private static final String NO_DATA = "_";
 
 	@Override
 	public Object coerceToBean(final Object arg0, final Component arg1) {
