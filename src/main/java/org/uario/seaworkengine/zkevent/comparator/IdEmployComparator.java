adce1e9170cf984b7a779c2cb340336de10e84bb
fdamore <francesco.damore@gmail.com>
2015-09-02 16:41:41 +0200

    Fixed order by matricola

--- src/main/java/org/uario/seaworkengine/zkevent/comparator/IdEmployComparator.java
+++ src/main/java/org/uario/seaworkengine/zkevent/comparator/IdEmployComparator.java
@@ -6,7 +6,7 @@ import org.uario.seaworkengine.model.DetailFinalSchedule;
 
 public class IdEmployComparator implements Comparator<DetailFinalSchedule> {
 
-	private final boolean	asc_dir;
+	private final boolean asc_dir;
 
 	public IdEmployComparator(final boolean asc) {
 		this.asc_dir = asc;
@@ -15,20 +15,50 @@ public class IdEmployComparator implements Comparator<DetailFinalSchedule> {
 	@Override
 	public int compare(final DetailFinalSchedule item_1, final DetailFinalSchedule item_2) {
 
-		final String id_1 = item_1.getEmployee_identification();
-		final String id_2 = item_2.getEmployee_identification();
+		final String id_1_info = item_1.getEmployee_identification();
+		final String id_2_info = item_2.getEmployee_identification();
+
+		Integer id_1 = null;
+		Integer id_2 = null;
+
+		try {
+			id_1 = Integer.parseInt(id_1_info);
+			id_2 = Integer.parseInt(id_2_info);
+		} catch (final NumberFormatException e) {
+			id_1 = null;
+			id_2 = null;
+		}
 
 		if ((id_1 != null) && (id_2 != null)) {
 
-			if (this.asc_dir) {
-				return id_1.compareTo(id_2);
-			} else {
-				return id_2.compareTo(id_1);
-			}
-		}
+			if ((id_1 != null) && (id_2 != null)) {
 
-		else {
-			return -1;
+				if (this.asc_dir) {
+					return id_1.compareTo(id_2);
+				} else {
+					return id_2.compareTo(id_1);
+				}
+			}
+
+			else {
+				return -1;
+			}
+
+		} else {
+
+			if ((id_1_info != null) && (id_2_info != null)) {
+
+				if (this.asc_dir) {
+					return id_1_info.compareTo(id_2_info);
+				} else {
+					return id_2_info.compareTo(id_1_info);
+				}
+			}
+
+			else {
+				return -1;
+			}
+
 		}
 
 	}
