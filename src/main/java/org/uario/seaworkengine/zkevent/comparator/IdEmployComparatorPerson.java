7e497356f180ed1d1291fe5245286f2e0315faf3
fdamore <francesco.damore@gmail.com>
2015-10-06 14:08:24 +0200

    Added Matricola at User List

---
+++ src/main/java/org/uario/seaworkengine/zkevent/comparator/IdEmployComparatorPerson.java
@@ -0,0 +1,66 @@
+package org.uario.seaworkengine.zkevent.comparator;
+
+import java.util.Comparator;
+
+import org.uario.seaworkengine.model.Person;
+
+public class IdEmployComparatorPerson implements Comparator<Person> {
+
+	private final boolean asc_dir;
+
+	public IdEmployComparatorPerson(final boolean asc) {
+		this.asc_dir = asc;
+	}
+
+	@Override
+	public int compare(final Person item_1, final Person item_2) {
+
+		final String id_1_info = item_1.getEmployee_identification();
+		final String id_2_info = item_2.getEmployee_identification();
+
+		Integer id_1 = null;
+		Integer id_2 = null;
+
+		try {
+			id_1 = Integer.parseInt(id_1_info);
+			id_2 = Integer.parseInt(id_2_info);
+		} catch (final NumberFormatException e) {
+			id_1 = null;
+			id_2 = null;
+		}
+
+		if ((id_1 != null) && (id_2 != null)) {
+
+			if ((id_1 != null) && (id_2 != null)) {
+
+				if (this.asc_dir) {
+					return id_1.compareTo(id_2);
+				} else {
+					return id_2.compareTo(id_1);
+				}
+			}
+
+			else {
+				return -1;
+			}
+
+		} else {
+
+			if ((id_1_info != null) && (id_2_info != null)) {
+
+				if (this.asc_dir) {
+					return id_1_info.compareTo(id_2_info);
+				} else {
+					return id_2_info.compareTo(id_1_info);
+				}
+			}
+
+			else {
+				return -1;
+			}
+
+		}
+
+	}
+
+}
