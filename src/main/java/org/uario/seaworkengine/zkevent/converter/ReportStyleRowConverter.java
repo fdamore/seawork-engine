3e1efa6626461d5dcb2b5eace2e508dbf1e8c491
Gabriele Lanza <g.lanza@uario.com>
2015-11-17 15:42:09 +0100

    Added style in report rows - Completed SEA-663

---
+++ src/main/java/org/uario/seaworkengine/zkevent/converter/ReportStyleRowConverter.java
@@ -0,0 +1,53 @@
+package org.uario.seaworkengine.zkevent.converter;
+
+import org.uario.seaworkengine.model.ReportItem;
+import org.uario.seaworkengine.utility.ReportItemTag;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zkplus.databind.TypeConverter;
+
+public class ReportStyleRowConverter implements TypeConverter {
+
+	@Override
+	public Object coerceToBean(final Object val, final Component comp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object coerceToUi(final Object arg0, final Component arg1) {
+		if (!(arg0 instanceof ReportItem) || (arg0 == null)) {
+			return "0";
+		}
+
+		final ReportItem source = (ReportItem) arg0;
+
+		if (source.getArgument() == null) {
+			return "";
+		}
+
+		if (source.getArgument().equals(ReportItemTag.Containers) || source.getArgument().equals(ReportItemTag.ContainerRZ_TW_SWS)
+				|| source.getArgument().equals(ReportItemTag.ContainerRZ_TW_MCT)) {
+			return "background-color:#FFFDCF";
+		} else if (source.getArgument().equals(ReportItemTag.Hands)) {
+			return "background-color:#F0CE8F";
+		} else if (source.getArgument().equals(ReportItemTag.HandsC_P)) {
+			return "background-color:#F1886B";
+		} else if (source.getArgument().equals(ReportItemTag.TaskHours) || source.getIsTaskROW()) {
+			return "background-color:#F4E4B3";
+		} else if (source.getArgument().equals(ReportItemTag.HandsOnDays) || source.getArgument().equals(ReportItemTag.MenOnHands)
+				|| source.getArgument().equals(ReportItemTag.ContainersOnMen) || source.getArgument().equals(ReportItemTag.Productivity)
+				|| source.getArgument().equals(ReportItemTag.ContainerOnHours) || source.getArgument().equals(ReportItemTag.ContainerOnHours)
+				|| source.getArgument().equals(ReportItemTag.ContainersOnHours)) {
+			return "background-color:#F4E4B3";
+		} else if (source.getArgument().contains(ReportItemTag.HandsC_shift)) {
+			return "background-color:#E9C6B2";
+		} else if (source.getArgument().contains(ReportItemTag.HandsP_shift)) {
+			return "background-color:#FFFDCF";
+		} else if (source.getArgument().contains(ReportItemTag.CustomerComplaint)) {
+			return "background-color:#F74537";
+		}
+		return "";
+
+	}
+
+}
