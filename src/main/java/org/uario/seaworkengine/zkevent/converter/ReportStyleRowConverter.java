acf9751b05c0b91d090b5772fc025ec4084dbef2
Gabriele Lanza <g.lanza@uario.com>
2015-11-24 12:23:59 +0100

    Added colors in print report - Improving SEA-671

--- src/main/java/org/uario/seaworkengine/zkevent/converter/ReportStyleRowConverter.java
+++ src/main/java/org/uario/seaworkengine/zkevent/converter/ReportStyleRowConverter.java
@@ -27,38 +27,38 @@ public class ReportStyleRowConverter implements TypeConverter {
 
 		if (source.getArgument().equals(ReportItemTag.Containers) || source.getArgument().equals(ReportItemTag.ContainerRZ_TW_SWS)
 				|| source.getArgument().equals(ReportItemTag.ContainerRZ_TW_MCT)) {
-			return "background-color:#FFFDCF";
+			return "containerStyleReport";
 		} else if (source.getArgument().equals(ReportItemTag.TaskTotalHours)) {
-			return "background-color:#FAD905";
+			return "taskTotalHoursStyleReport";
 		} else if (source.getArgument().equals(ReportItemTag.Hands)) {
-			return "background-color:#F0CE8F";
+			return "handsStyleReport";
 		} else if (source.getArgument().equals(ReportItemTag.HandsC_P)) {
-			return "background-color:#F1886B";
+			return "handsC_PStyleReport";
 		} else if (source.getArgument().equals(ReportItemTag.TaskHours) || source.getIsTaskROW()) {
-			return "background-color:#F4E4B3";
+			return "taskHoursStyleReport";
 		} else if (source.getArgument().equals(ReportItemTag.HandsOnDays) || source.getArgument().equals(ReportItemTag.MenOnHands)
 				|| source.getArgument().equals(ReportItemTag.Productivity) || source.getArgument().equals(ReportItemTag.ContainersOnHours)) {
-			return "background-color:#F4E4B3";
+			return "handsOnDaysStyleReport";
 		} else if (source.getArgument().contains(ReportItemTag.HandsC_shift)) {
-			return "background-color:#E9C6B2";
+			return "handsC_shiftStyleReport";
 		} else if (source.getArgument().contains(ReportItemTag.HandsP_shift)) {
-			return "background-color:#FFFDCF";
+			return "handsP_shiftStyleReport";
 		} else if (source.getArgument().contains(ReportItemTag.CustomerComplaint)) {
-			return "background-color:#F74537";
+			return "customerComplaintStyleReport";
 		} else if (source.getArgument().equals(ReportItemTag.WindyDay)) {
-			return "background-color:#DEDEDE";
+			return "windyDayStyleReport";
 		} else if (source.getArgument().equals(ReportItemTag.ShipNumberComplete) || source.getArgument().equals(ReportItemTag.ShipNumberTwist)) {
-			return "background-color:##FFFFFF";
+			return "shipNumberStyleReport";
 		} else if (source.getIsService() != null) {
 			final int i = source.getIsService() % 2;
 			if (i == 0) {
-				return "background-color:#D4A190";
+				return "isServiceStyleReport";
 			}
-			return "background-color:#EDD6CE";
+			return "isServiceStyleReportd";
 		} else if (source.getArgument().equals(ReportItemTag.ContainersOnMen) || source.getArgument().equals(ReportItemTag.ContainerOnHours)) {
-			return "background-color:#FFD700";
+			return "containersOnMenStyleReport";
 		} else if (source.getArgument().equals(ReportItemTag.Service_TimeWorkTotal)) {
-			return "background-color:#FFD700";
+			return "service_TimeWorkTotalStyleReport";
 		}
 		return "";
 
