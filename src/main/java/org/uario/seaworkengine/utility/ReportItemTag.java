fc7ebf33179f47ac4b787eedd15a2222c2215ad5
francesco <francesco.damore@gmail.com>
2017-07-19 15:42:32 +0200

    removed "fatturate"

--- src/main/java/org/uario/seaworkengine/utility/ReportItemTag.java
+++ src/main/java/org/uario/seaworkengine/utility/ReportItemTag.java
@@ -19,13 +19,13 @@ public interface ReportItemTag {
 	public static String	Service_HoursMan		= "Totale ore Lavorate ";
 	public static String	Service_NumberOfMan		= "Totale uomini per servizio ";
 	public static String	Service_NumberOfService	= "Numero di servizi ";
-	public static String	Service_TimeWork		= "Totale ore Fatturate Nette per servizio ";
-	public static String	Service_TimeWorkTotal	= "Totale ore Fatturate Nette per servizi";
+	public static String	Service_TimeWork		= "Totale ore Nette per servizio ";
+	public static String	Service_TimeWorkTotal	= "Totale ore Nette per servizi";
 	public static String	ShipNumberComplete		= "Numero navi - Completa";
 	public static String	ShipNumberTwist			= "Numero navi - TW";
 	public static String	TaskHours				= "Ore lavorate per mansione";
 	public static String	TaskTotalHours			= "Totale ore Lavorate per mansioni";
 	public static String	TaskTotalHoursRZ_PP		= "Totale ore RZ-PP";
 	public static String	WindyDay				= "Giorni disagio vento";
-
+	
 }
