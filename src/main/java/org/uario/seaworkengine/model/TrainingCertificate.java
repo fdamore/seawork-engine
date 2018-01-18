dda4fd00a5a285ea682fe559579084209231d14a
Francesco D'Amore <francesco.damore@gmail.com>
2018-01-18 19:24:34 +0100

    Chamge on editor "Formazine"

--- src/main/java/org/uario/seaworkengine/model/TrainingCertificate.java
+++ src/main/java/org/uario/seaworkengine/model/TrainingCertificate.java
@@ -4,44 +4,44 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class TrainingCertificate implements Serializable {
-	
+
 	/**
 	 *
 	 */
 	private static final long	serialVersionUID	= 1L;
-	
+
 	private Date				certificate_date;
-	
+
 	private String				description;
-	
-	private Integer				duration;
-	
+
+	private Double				duration;
+
 	private Date				end_class;
-	
+
 	private Date				expiration_date;
-	
+
 	private Integer				id;
 
 	private String				note;
 
 	private Date				start_class;
-	
+
 	private String				title;
-	
+
 	private String				trainer;
-	
+
 	private String				trainer_type;
-	
+
 	private String				training_level;
-	
+
 	private String				training_task;
-	
+
 	private String				tutor;
-	
+
 	private String				typ;
-	
+
 	private Integer				user_id;
-	
+
 	public Date getCertificate_date() {
 		return this.certificate_date;
 	}
@@ -49,125 +49,125 @@ public class TrainingCertificate implements Serializable {
 	public String getDescription() {
 		return this.description;
 	}
-	
-	public Integer getDuration() {
+
+	public Double getDuration() {
 		return this.duration;
 	}
-	
+
 	public Date getEnd_class() {
 		return this.end_class;
 	}
-	
+
 	public Date getExpiration_date() {
 		return this.expiration_date;
 	}
-	
+
 	public Integer getId() {
 		return this.id;
 	}
-	
+
 	public String getNote() {
 		return this.note;
 	}
-	
+
 	public Date getStart_class() {
 		return this.start_class;
 	}
-	
+
 	public String getTitle() {
 		return this.title;
 	}
-	
+
 	public String getTrainer() {
 		return this.trainer;
 	}
-	
+
 	public String getTrainer_type() {
 		return this.trainer_type;
 	}
-	
+
 	public String getTraining_level() {
 		return this.training_level;
 	}
-	
+
 	public String getTraining_task() {
 		return this.training_task;
 	}
-	
+
 	public String getTutor() {
 		return this.tutor;
 	}
-	
+
 	public String getTyp() {
 		return this.typ;
 	}
-	
+
 	public Integer getUser_id() {
 		return this.user_id;
 	}
-	
+
 	public void setCertificate_date(final Date certificate_date) {
 		this.certificate_date = certificate_date;
 	}
-	
+
 	public void setDescription(final String description) {
 		this.description = description;
 	}
-	
-	public void setDuration(Integer duration) {
+
+	public void setDuration(final Double duration) {
 		this.duration = duration;
 	}
-	
-	public void setEnd_class(Date end_class) {
+
+	public void setEnd_class(final Date end_class) {
 		this.end_class = end_class;
 	}
-	
+
 	public void setExpiration_date(final Date expiration_date) {
 		this.expiration_date = expiration_date;
 	}
-	
+
 	public void setId(final Integer id) {
 		this.id = id;
 	}
-	
+
 	public void setNote(final String note) {
 		this.note = note;
 	}
-	
-	public void setStart_class(Date start_class) {
+
+	public void setStart_class(final Date start_class) {
 		this.start_class = start_class;
 	}
-	
+
 	public void setTitle(final String title) {
 		this.title = title;
 	}
-	
+
 	public void setTrainer(final String trainer) {
 		this.trainer = trainer;
 	}
-	
+
 	public void setTrainer_type(final String trainer_type) {
 		this.trainer_type = trainer_type;
 	}
-	
+
 	public void setTraining_level(final String training_level) {
 		this.training_level = training_level;
 	}
-	
+
 	public void setTraining_task(final String training_task) {
 		this.training_task = training_task;
 	}
-	
-	public void setTutor(String tutor) {
+
+	public void setTutor(final String tutor) {
 		this.tutor = tutor;
 	}
-	
-	public void setTyp(String typ) {
+
+	public void setTyp(final String typ) {
 		this.typ = typ;
 	}
-	
+
 	public void setUser_id(final Integer user_id) {
 		this.user_id = user_id;
 	}
-	
+
 }
--- src/main/java/org/uario/seaworkengine/zkevent/UserDetailsComposerTrainingCertificate.java
+++ src/main/java/org/uario/seaworkengine/zkevent/UserDetailsComposerTrainingCertificate.java
@@ -20,15 +20,15 @@ import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zul.Combobox;
 import org.zkoss.zul.Datebox;
+import org.zkoss.zul.Doublebox;
 import org.zkoss.zul.Filedownload;
-import org.zkoss.zul.Intbox;
 import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Listbox;
 import org.zkoss.zul.Messagebox;
 import org.zkoss.zul.Textbox;
 
 public class UserDetailsComposerTrainingCertificate extends SelectorComposer<Component> {
-	
+
 	/**
 	 *
 	 */
@@ -39,67 +39,67 @@ public class UserDetailsComposerTrainingCertificate extends SelectorComposer<Com
 
 	@Wire
 	private Textbox					description;
-	
+
 	@Wire
 	private Datebox					expiration_date;
-	
+
 	@Wire
 	private Textbox					full_text_search;
-	
+
 	@Wire
 	private Component				grid_details;
-	
+
 	private final Logger			logger				= Logger.getLogger(UserDetailsComposerTrainingCertificate.class);
 
 	@Wire
 	private Textbox					note;
-	
+
 	private Person					person_selected;
-	
+
 	// status ADD or MODIFY
 	private boolean					status_add			= false;
-	
+
 	@Wire
 	private Listbox					sw_list;
-	
+
 	@Wire
 	private Textbox					title;
-	
+
 	@Wire
 	private Textbox					trainer;
-	
+
 	@Wire
 	private Textbox					trainer_type;
-	
+
 	@Wire
-	private Intbox					training_duration;
-	
+	private Doublebox				training_duration;
+
 	@Wire
 	private Datebox					training_end_class;
 
 	@Wire
 	private Textbox					training_level;
-	
+
 	@Wire
 	private Datebox					training_start_class;
 
 	@Wire
 	private Textbox					training_task;
-	
+
 	@Wire
 	private Combobox				training_tutor;
-	
+
 	@Wire
 	private Combobox				training_typ;
-	
+
 	// dao interface
 	private TrainingCertificateDAO	trainingCertificateDAO;
-	
+
 	@Listen("onClick = #sw_add")
 	public void addItem() {
-		
+
 		this.status_add = true;
-		
+
 		this.certificate_date.setValue(null);
 		this.expiration_date.setValue(null);
 		this.title.setValue("");
@@ -117,164 +117,164 @@ public class UserDetailsComposerTrainingCertificate extends SelectorComposer<Com
 		this.training_duration.setValue(null);
 
 		this.note.setValue(null);
-		
+
 	}
-	
+
 	private void deleteItemToUser() {
-		
+
 		if (this.sw_list.getSelectedItem() == null) {
 			return;
 		}
-		
+
 		if (this.person_selected == null) {
 			return;
 		}
-		
+
 		final TrainingCertificate item = this.sw_list.getSelectedItem().getValue();
-		
+
 		this.trainingCertificateDAO.removeTrainingCertificate(item.getId());
-		
+
 		// Refresh list task
 		this.setInitialView();
-		
+
 	}
-	
+
 	@Override
 	public void doFinally() throws Exception {
-		
+
 		this.getSelf().addEventListener(ZkEventsTag.onShowUsers, new EventListener<Event>() {
-			
+
 			@Override
 			public void onEvent(final Event arg0) throws Exception {
-				
+
 				// get selected person
 				if ((arg0.getData() == null) || !(arg0.getData() instanceof Person)) {
 					return;
 				}
 				UserDetailsComposerTrainingCertificate.this.person_selected = (Person) arg0.getData();
-				
+
 				// get the dao
 				UserDetailsComposerTrainingCertificate.this.trainingCertificateDAO = (TrainingCertificateDAO) SpringUtil
 						.getBean(BeansTag.TRAINING_CERTIFICATE_DAO);
-				
+
 				UserDetailsComposerTrainingCertificate.this.setInitialView();
-				
+
 			}
 		});
-		
+
 	}
-	
+
 	@Listen("onClick = #user_csv")
 	public void downloadCSV_user_csv() {
-		
+
 		if ((this.person_selected == null) || (this.person_selected.getId() == null)) {
 			return;
 		}
-		
+
 		final List<TrainingCertificate> list = this.trainingCertificateDAO.loadTrainingCertificate(null, null, null, this.person_selected.getId());
 		final StringBuilder builder = UtilityCSV.downloadCSV_user_formazione(list);
 		Filedownload.save(builder.toString(), "application/text", "info_formazione.csv");
-		
+
 	}
-	
+
 	@Listen("onClick = #sw_link_edit")
 	public void modifyItem() {
-		
+
 		this.status_add = false;
-		
+
 		// get selected item
 		final TrainingCertificate item = this.sw_list.getSelectedItem().getValue();
-		
+
 		// set info about grid
 		if (this.person_selected == null) {
 			return;
 		}
-		
+
 		if (item == null) {
 			return;
 		}
-		
+
 		if ((this.certificate_date.getValue() != null) && (this.expiration_date.getValue() != null)
 				&& this.certificate_date.getValue().after(this.expiration_date.getValue())) {
 			return;
 		}
-		
+
 		this.certificate_date.setValue(item.getCertificate_date());
 		this.expiration_date.setValue(item.getExpiration_date());
 		this.description.setValue(item.getDescription());
 		this.title.setValue(item.getTitle());
-		
+
 		String trainerType = item.getTrainer_type();
-		
+
 		if ((trainerType != null) && trainerType.equals("--")) {
 			trainerType = null;
 		}
-		
+
 		this.trainer_type.setValue(trainerType);
 		this.trainer.setValue(item.getTrainer());
 		this.training_task.setValue(item.getTraining_task());
 		this.training_level.setValue(item.getTraining_level());
 		this.note.setValue(item.getNote());
-		
+
 		this.training_typ.setValue(item.getTyp());
 		this.training_tutor.setValue(item.getTutor());
 		this.training_start_class.setValue(item.getStart_class());
 		this.training_end_class.setValue(item.getEnd_class());
-		
+
 		this.training_duration.setValue(item.getDuration());
-		
+
 	}
-	
+
 	@Listen("onClick = #ok_command")
 	public void okCommand() {
-		
+
 		if (this.person_selected == null) {
 			return;
 		}
-		
+
 		if ((this.certificate_date.getValue() != null) && (this.expiration_date.getValue() != null)
 				&& this.certificate_date.getValue().after(this.expiration_date.getValue())) {
 			return;
 		}
-		
+
 		if (this.status_add) {
-			
+
 			final TrainingCertificate item = new TrainingCertificate();
-			
+
 			// setup item with values
 			this.setupItemWithValues(item);
-			
+
 			this.trainingCertificateDAO.createTrainingCertificate(item);
-			
+
 		} else {
-			
+
 			// get selected item
 			final TrainingCertificate item = this.sw_list.getSelectedItem().getValue();
 			if (item == null) {
 				return;
 			}
-			
+
 			// add values to the items
 			this.setupItemWithValues(item);
-			
+
 			this.trainingCertificateDAO.updateTrainingCertificate(item);
 		}
-		
+
 		// Refresh list task
 		this.setInitialView();
-		
+
 	}
-	
+
 	@Listen("onClick = #sw_link_delete")
 	public void removeItem() {
-		
+
 		final Map<String, String> params = new HashMap();
 		params.put("sclass", "mybutton Button");
-		
+
 		final Messagebox.Button[] buttons = new Messagebox.Button[2];
 		buttons[0] = Messagebox.Button.OK;
 		buttons[1] = Messagebox.Button.CANCEL;
-		
+
 		Messagebox.show("Vuoi cancellare la voce selezionata?", "CONFERMA CANCELLAZIONE", buttons, null, Messagebox.EXCLAMATION, null,
 				new EventListener() {
 					@Override
@@ -286,52 +286,52 @@ public class UserDetailsComposerTrainingCertificate extends SelectorComposer<Com
 						}
 					}
 				}, params);
-		
+
 	}
-	
+
 	@Listen("onOK = #full_text_search")
 	public void searchText() {
 		if (this.person_selected == null) {
 			return;
 		}
-		
+
 		final List<TrainingCertificate> list = this.trainingCertificateDAO.loadTrainingCertificate(null, this.full_text_search.getValue(), null,
 				this.person_selected.getId());
 		this.sw_list.setModel(new ListModelList<>(list));
-		
+
 		this.grid_details.setVisible(false);
 	}
-	
+
 	@Listen("onClick = #sw_refresh_list")
 	public void setInitialView() {
-		
+
 		this.full_text_search.setValue(null);
-		
+
 		if (this.person_selected == null) {
 			return;
 		}
-		
+
 		final List<TrainingCertificate> list = this.trainingCertificateDAO.loadTrainingCertificate(null, this.full_text_search.getValue(), null,
 				this.person_selected.getId());
 		this.sw_list.setModel(new ListModelList<>(list));
-		
+
 		this.grid_details.setVisible(false);
 	}
-	
+
 	private void setupItemWithValues(final TrainingCertificate item) {
-		
+
 		item.setCertificate_date(this.certificate_date.getValue());
 		item.setExpiration_date(this.expiration_date.getValue());
 		item.setDescription(this.description.getValue());
 		item.setTitle(this.title.getValue());
 		item.setUser_id(this.person_selected.getId());
-		
+
 		String trainerType = this.trainer_type.getValue();
-		
+
 		if ((trainerType != null) && trainerType.equals("--")) {
 			trainerType = null;
 		}
-		
+
 		item.setTrainer_type(trainerType);
 		item.setTrainer(this.trainer.getValue());
 		item.setTraining_task(this.training_task.getValue());
@@ -342,37 +342,37 @@ public class UserDetailsComposerTrainingCertificate extends SelectorComposer<Com
 		item.setTutor(this.training_tutor.getValue());
 		item.setStart_class(this.training_start_class.getValue());
 		item.setEnd_class(this.training_end_class.getValue());
-		
+
 		item.setDuration(this.training_duration.getValue());
 
 	}
-	
+
 	@Listen("onClick = #show_expirated")
 	public void showExpiratedCertificates() {
-		
+
 		if (this.person_selected == null) {
 			return;
 		}
-		
+
 		final List<TrainingCertificate> list = this.trainingCertificateDAO.loadTrainingCertificate(null, this.full_text_search.getValue(), true,
 				this.person_selected.getId());
 		this.sw_list.setModel(new ListModelList<>(list));
-		
+
 		this.grid_details.setVisible(false);
 	}
-	
+
 	@Listen("onClick = #show_valid")
 	public void showValidCertificates() {
-		
+
 		if (this.person_selected == null) {
 			return;
 		}
-		
+
 		final List<TrainingCertificate> list = this.trainingCertificateDAO.loadTrainingCertificate(null, this.full_text_search.getValue(), false,
 				this.person_selected.getId());
 		this.sw_list.setModel(new ListModelList<>(list));
-		
+
 		this.grid_details.setVisible(false);
 	}
-	
+
 }
