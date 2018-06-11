c984df38ca7d6b4a6704fd07fd811181805b6e3c
francesco <francesco@undefined>
2018-06-11 20:00:15 +0200

    Fix SEA-799

--- src/main/java/org/uario/seaworkengine/web/zkevent/MobileComposer.java
+++ src/main/java/org/uario/seaworkengine/web/zkevent/MobileComposer.java
@@ -10,6 +10,8 @@ import org.uario.seaworkengine.web.services.IWebServiceController;
 import org.uario.seaworkengine.web.services.handler.InitialSchedule;
 import org.uario.seaworkengine.web.services.handler.InitialScheduleSingleDetail;
 import org.zkoss.bind.annotation.AfterCompose;
+import org.zkoss.bind.annotation.BindingParam;
+import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.ContextParam;
 import org.zkoss.bind.annotation.ContextType;
 import org.zkoss.bind.annotation.NotifyChange;
@@ -18,9 +20,9 @@ import org.zkoss.zk.ui.Component;
 
 public class MobileComposer {
 
-	private IWebServiceController				service;
+	private IWebServiceController service;
 
-	private List<InitialScheduleSingleDetail>	users;
+	private List<InitialScheduleSingleDetail> users;
 
 	public List<InitialScheduleSingleDetail> getUsers() {
 		return this.users;
@@ -30,12 +32,13 @@ public class MobileComposer {
 	public void init(@ContextParam(ContextType.COMPONENT) final Component component) throws Exception {
 		this.service = (IWebServiceController) SpringUtil.getBean(BeansTag.WEBCONTROLLER);
 
-		this.refresh();
+		this.refresh(null);
 
 	}
 
+	@Command
 	@NotifyChange({ "users" })
-	public void refresh() {
+	public void refresh(@BindingParam("shift_no") final Integer shift_no) {
 		if (this.users != null) {
 			this.users.clear();
 		} else {
@@ -51,6 +54,17 @@ public class MobileComposer {
 			}
 
 			for (final DetailInitialSchedule detail : insch.getDetail_schedule()) {
+
+				if (detail.getShift() == null) {
+					continue;
+				}
+
+				// filter on shift
+				if (shift_no != null) {
+					if (!shift_no.equals(detail.getShift())) {
+						continue;
+					}
+				}
 
 				final InitialScheduleSingleDetail itm = new InitialScheduleSingleDetail();
 				itm.setDetail_schedule(detail);
