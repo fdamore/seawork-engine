5cee6607ca2f04c97081758557b311e8259c1191
francesco <francesco@undefined>
2019-01-06 16:05:35 +0100

    Improve loked system

---
+++ src/main/java/org/uario/seaworkengine/zkevent/converter/DateWithTimeConverter.java
@@ -0,0 +1,31 @@
+package org.uario.seaworkengine.zkevent.converter;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zkplus.databind.TypeConverter;
+
+public class DateWithTimeConverter implements TypeConverter {
+
+	SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+
+	@Override
+	public Object coerceToBean(final Object arg0, final Component arg1) {
+
+		return null;
+	}
+
+	@Override
+	public Object coerceToUi(final Object arg0, final Component arg1) {
+
+		if (!(arg0 instanceof Date) || (arg0 == null)) {
+			return arg0;
+		}
+
+		final Date dt = (Date) arg0;
+
+		return this.format.format(dt);
+
+	}
+}
